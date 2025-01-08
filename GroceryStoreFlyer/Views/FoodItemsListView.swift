f52f27b3292d00af6d65ae15ee4c9d7ad5cf54a6
Ethan Chen <echen@lcs.on.ca>
2025-01-08 10:42:57 -0500

    added assets, improved interface by showing amountperpackage, price, and made a variable called priceper100g

--- GroceryStoreFlyer/Views/FoodItemsListView.swift
+++ GroceryStoreFlyer/Views/FoodItemsListView.swift
@@ -9,26 +9,39 @@ import SwiftUI
 
 struct FoodItemsListView: View {
     let departmentToShow: Department
+
     var body: some View {
             List(departmentToShow.items) {currentItem in
+                let pricePerHundredGrams = currentItem.price / (currentItem.amountPerPackage/100)
                 HStack (alignment: .top){
                     VStack (alignment: .leading){
                         Text(currentItem.name)
                             .bold()
                             .font(.system(size: 30))
-                        Text("$\(String(format: "%.2f", currentItem.price))")
-                            .font(.system(size: 30))
+                        HStack{
+                            Text("$\(String(format: "%.2f", currentItem.price)) ")
+                                .font(.system(size: 30))
+                                .bold()
+                                .foregroundColor(.blue)
+                            Text("per \(String(format: "%.0f",currentItem.amountPerPackage))g")
+                        }
+                        Text("$\(String(format: "%.2f", pricePerHundredGrams)) per 100g")
+                            .foregroundColor(.red)
+                        
+                            
                     } .padding(.trailing,5)
+                        
                     Spacer()
                     
                 Image(currentItem.image)
                     .resizable()
-                    .frame(width:100, height:100)
+                    .frame(width:120, height:120)
                 }
                     
             }
+            .padding(.top,10)
                 .navigationTitle(departmentToShow.name)
-
+        
     }
 }
 
